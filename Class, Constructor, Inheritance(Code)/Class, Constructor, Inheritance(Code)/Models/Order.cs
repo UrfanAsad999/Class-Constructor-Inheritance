7fcf4e0b62de68c3869bb265bdba2db596da933f
UrfanAsad999 <999asad9@gmail.com>
2026-03-17 23:22:38 +0400

    Add files via upload

---
+++ Class, Constructor, Inheritance(Code)/Class, Constructor, Inheritance(Code)/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class__Constructor__Inheritance_Code_.Models
+{
+    internal class Order
+    {
+    }
+}
